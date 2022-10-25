e08ea2105d70ed91e63b2ad2fbf63639a4621e12
Melvine Juma <juma.study@gmail.com>
2022-10-25 14:41:32 +0300

    App connection to Atlas

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.MONGODB_PASSWORD
+);
+
+//  CONNECT TO LOCAL MONGODB (HOSTED LOCALLY)
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log('DB Connection succesful');
+//   });
+
+// CONNECT TO MONGODB (HOSTED DB VERSION)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connection succesful');
+  });
+
 //START UP SERVER
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
