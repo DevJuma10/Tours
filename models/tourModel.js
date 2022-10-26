2bb4dcc6ac160fbaac68237066cde354066ae44b
DevJuma10 <juma.study@gmail.com>
2022-10-27 00:48:57 +0300

    Tours Schema Complete

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,17 +6,67 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour must have a name'],
     unique: true,
+    trim: true,
   },
 
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour muust have a duration'],
+  },
+
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour Must Have a Maximum Group Size '],
+  },
+
+  difficulty: {
+    type: String,
+    required: [true, 'Tour Must Have a Difficulty Level'],
+  },
+
+  ratingsAverage: {
     type: Number,
     default: 3.5,
   },
 
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+
   price: {
     type: Number,
-    required: true,
+    required: [true, 'Tour Must Have a Price '],
   },
+
+  priceDiscount: {
+    type: Number,
+  },
+
+  summary: {
+    type: String,
+    required: [true, 'Tour Must Have a Summary of the tour'],
+    trim: true,
+  },
+
+  description: {
+    type: String,
+    trim: true,
+  },
+
+  imageCover: {
+    type: String,
+    required: [true, 'Tour Must Have an image cover'],
+  },
+
+  images: [String],
+
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  startDates: [Date],
 
   premium: {
     type: Boolean,
