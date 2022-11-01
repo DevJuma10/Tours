57d57cd629e12eec3d0f4aabe82c1578e8bee893
DevJuma10 <juma.study@gmail.com>
2022-11-01 23:08:39 +0300

    Pagination

--- controlers/tourControler.js
+++ controlers/tourControler.js
@@ -75,6 +75,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    //page=2&limit=10  1-10. 11-20
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This Page Does NOT exist');
+      }
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
