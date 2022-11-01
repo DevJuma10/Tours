635e1fc6c5f1e03d8a77cae3f575e983e04e66f1
DevJuma10 <juma.study@gmail.com>
2022-11-01 16:22:50 +0300

    Implemented Tour sorting feature

--- controlers/tourControler.js
+++ controlers/tourControler.js
@@ -48,12 +48,24 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'limit', 'sort', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 2) ADVANCED FILTERING
+    // ADVANCED FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) SORTING
+    if (req.query.sort) {
+      query = query.sort(req.query.sort);
+      const sortBy = req.query.sort.split(',').join(' ');
+
+      query = query.sort(sortBy);
+
+      //  Default fallback in the case sorting parameters are not included
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
