0a39dc80b49f6051fd4e61fd0aa855f2fffc7f43
DevJuma10 <juma.study@gmail.com>
2022-11-04 03:41:55 +0300

    Virtual properties : durationweeks

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,78 +1,92 @@
 const mongoose = require('mongoose');
 
 //  CREATE SCHEMA
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+
+    duration: {
+      type: Number,
+      required: [true, 'Tour muust have a duration'],
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour Must Have a Maximum Group Size '],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, 'Tour Must Have a Difficulty Level'],
+    },
+
+    ratingsAverage: {
+      type: Number,
+      default: 3.5,
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'Tour Must Have a Price '],
+    },
+
+    priceDiscount: {
+      type: Number,
+    },
+
+    summary: {
+      type: String,
+      required: [true, 'Tour Must Have a Summary of the tour'],
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, 'Tour Must Have an image cover'],
+    },
+
+    images: [String],
+
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+
+    startDates: [Date],
+
+    premium: {
+      type: Boolean,
+      default: false,
+    },
   },
 
-  duration: {
-    type: Number,
-    required: [true, 'Tour muust have a duration'],
-  },
+  //  OPTIONS
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'Tour Must Have a Maximum Group Size '],
-  },
+// CREATING VIRTUAL PROPERTIES
 
-  difficulty: {
-    type: String,
-    required: [true, 'Tour Must Have a Difficulty Level'],
-  },
-
-  ratingsAverage: {
-    type: Number,
-    default: 3.5,
-  },
-
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-
-  price: {
-    type: Number,
-    required: [true, 'Tour Must Have a Price '],
-  },
-
-  priceDiscount: {
-    type: Number,
-  },
-
-  summary: {
-    type: String,
-    required: [true, 'Tour Must Have a Summary of the tour'],
-    trim: true,
-  },
-
-  description: {
-    type: String,
-    trim: true,
-  },
-
-  imageCover: {
-    type: String,
-    required: [true, 'Tour Must Have an image cover'],
-  },
-
-  images: [String],
-
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-
-  startDates: [Date],
-
-  premium: {
-    type: Boolean,
-    default: false,
-  },
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 //  CREATING A SIMPLE TOUR MODEL
