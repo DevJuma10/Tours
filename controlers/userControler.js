b75860697967ae88145c51c22b32179b174022d3
DevJuma10 <juma.study@gmail.com>
2023-01-31 17:46:41 +0300

    User/updateMe endpoint retouch

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -29,58 +29,28 @@ exports.getAllUsers = catchAsync(async (req, res) => {
   });
 });
 
-// exports.updateMe = catchAsync(async (req, res, next) => {
-//   // Create an error if user post password data
-//   if (req.body.password || req.body.passwordConfirm) {
-//     return next(
-//       new AppError(
-//         'This route is not for password update. Please use /updateMyPassword',
-//         400
-//       )
-//     );
-//   }
-//   // Update user document
-//   //fileter out unwanted field names not allowed to be updated
-//   const filteredBody = filterObj(req.body, 'name', 'email');
-//   const userx = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-//     new: true,
-//     runValidators: true,
-//   });
-
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       user: userx,
-//     },
-//   });
-// });
-
 exports.updateMe = catchAsync(async (req, res, next) => {
-  // 1) Create error if user POSTs password data
+  // Create an error if user post password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
-        'This route is not for password updates. Please use /updateMyPassword.',
+        'This route is not for password update. Please use /updateMyPassword',
         400
       )
     );
   }
-
-  // 2) Filtered out unwanted fields names that are not allowed to be updated
-  // const filteredBody = filterObj(req.body, 'name', 'email');
-
-  // 3) Update user document
-  // const updatedUser = await User.findOneAndUpdate(req.user._id, filteredBody, {
-  //   new: true,
-  //   runValidators: true,
-  // });
-
-  const testUser = await User.findByIdAndUpdate(req.user.id, { name: 'Jux' });
+  // Update user document
+  //fileter out unwanted field names not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     status: 'success',
     data: {
-      user: testUser,
+      user: updatedUser,
     },
   });
 });
