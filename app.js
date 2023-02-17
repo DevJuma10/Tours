47d4503ed4d9e9f14178fa0373c09d4d5a5ce22f
DevJuma10 <juma.study@gmail.com>
2023-02-17 15:05:47 +0300

    Extending Base Template

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6b9829ca59bac487f4a74136371ffb49a3089ecfe4942a0cb90d7d73da5692f
-size 2672
+oid sha256:8e0c80485c0ae8069ad5a89f95b8291b7c12f21daf9fc9e5cfff2b480fb4c63c
+size 2897
