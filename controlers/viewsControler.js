a984a8667d4551e2906107585c8782710e09f2b8
DevJuma10 <juma.study@gmail.com>
2023-02-20 09:09:45 +0300

    Tour Overview Page

--- controlers/viewsControler.js
+++ controlers/viewsControler.js
@@ -1,8 +1,19 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1. Get Tour data from collection
+  const tours = await Tour.find();
+
+  //2 Build Template
+
+  //render template using data form 1
+
   res.status(200).render('overview', {
-    title: 'All Tours',
+    title: 'Home',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
